ee11d366eb92d50e77532c7d12133ba8d93df0fc
novaTopFlex <78602536+novaTopFlex@users.noreply.github.com>
2024-04-26 19:59:48 -0400

    Create TkinterTest.py

---
+++ TkinterTest.py
@@ -0,0 +1,11 @@
+# This demonstration should result in a simple Tkinter window with a button specifically for opening new Tkinter windows.
+import tkinter
+
+root = tkinter.Tk()
+
+def newWindow():
+  rootN = tkinter.Tk()
+  rootN.mainloop()
+
+button = tkinter.Button(root, text="New Window", command=newWindow)
+root.mainloop()
