abc3e3195773d30cb6619700946a3b1eace9dd42
novaTopFlex <78602536+novaTopFlex@users.noreply.github.com>
2024-04-27 16:32:22 -0400

    Create DearPyGUI.py

---
+++ DearPyGUI.py
@@ -0,0 +1,8 @@
+import dearpygui
+import dearpygui.dearpygui
+
+dearpygui.dearpygui.create_context()
+dearpygui.dearpygui.create_viewport()
+dearpygui.dearpygui.setup_dearpygui()
+dearpygui.show_viewport()
+dearpygui.start_dearpygui()
